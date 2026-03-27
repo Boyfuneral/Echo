d9ebcf43a81933e795ee7aabe2d6d8cae4e6abfb
Chelsey <chelseysilva15@gmail.com>
2026-03-26 22:32:14 -0500

    Progress
    Progress on first task

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject savePanel;
+
+    public void StartNewGame()
+    {
+        savePanel.SetActive(true);
+    }
+
+    public void ContinueGame()
+    {
+        savePanel.SetActive(true);
+    }
+
+    public void SelectSlot1()
+    {
+        Debug.Log("Slot 1 selected");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void SelectSlot2()
+    {
+        Debug.Log("Slot 2 Selected");
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void CloseSavePanel()
+    {
+        savePanel.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Settings clicked");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit clicked");
+    }
+}
